848ab6e3eb125db5186bfa819987b65bf7f75253
atk <aditilearninghub@gmail.com>
2023-02-03 16:19:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+
+const App=()=>{
+  return(
+    <View>
+      <Text style={{fontSize:30}}>Hello !!!!!!!!!!!!! </Text>
+      <Text style={{fontSize:20}}>I am Creating my first React-Native Project</Text>
+    </View>
+
+  );
+
+};
+
+export default App;
